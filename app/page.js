2a3fb6ac66b83174f2bce7bc1f8eff50a83621ec
Arthur Duboks <duboks.arthur@gmail.com>
2024-01-10 17:02:51 -0500

    add clerk auth

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import Link from "next/link";
+import { CiLocationArrow1 } from "react-icons/ci";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main>
-      <h1>Hello!</h1>
-    </main>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-6xl font-bold text-primary">
+            GPT <span className="underline">Creative</span>
+          </h1>
+          <p className="py-6 text-lg leading-loose">
+            Your new marketing assistant. Powered by OpenAI. Create content, and
+            more!
+          </p>
+          <Link href="/chat" className="btn btn-secondary">
+            Get Started
+            <CiLocationArrow1 className="text-lg" />
+          </Link>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default HomePage;
