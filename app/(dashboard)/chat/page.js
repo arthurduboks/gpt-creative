1abb8832ad23b72415a2c44188b9391ea3612a9d
Arthur Duboks <duboks.arthur@gmail.com>
2024-01-11 17:45:31 -0500

    chat UI

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,5 +1,11 @@
+import Chat from "@/app/components/Chat";
+
 const ChatPage = () => {
-  return <div>Chat Page</div>;
+  return (
+    <>
+      <Chat />
+    </>
+  );
 };
 
 export default ChatPage;
